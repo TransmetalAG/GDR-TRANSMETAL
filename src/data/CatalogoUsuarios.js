b7429a56f5eeabdcbea1390efdfdda9890386962
TransmetalAG <transmetalag@gmail.com>
2026-07-22 12:05:01 -0600

    Create CatalogoUsuarios.js

---
+++ src/data/CatalogoUsuarios.js
@@ -0,0 +1,22 @@
+export const usuarios = [
+  {
+    nombre: "Pablo Hernández",
+    password: "10002949",
+    rol: "Administrador",
+  },
+  {
+    nombre: "José Suruy",
+    password: "10000200",
+    rol: "Auditor",
+  },
+  {
+    nombre: "Ricardo Estrada",
+    password: "10003043",
+    rol: "Auditor",
+  },
+  {
+    nombre: "Mantenimiento",
+    password: "MantenimientoTransmetal",
+    rol: "Mantenimiento",
+  },
+];
